fb826f597e722e8e50acbee1895c0bc537f1ea9b
superheropug <71048116+superheropug@users.noreply.github.com>
2026-05-02 22:25:06 -0500

    main.css wasn't being used kek

--- Website/common-ground/src/main.ts
+++ Website/common-ground/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./assets/reset.css";
+import "./assets/theme.css";
 
 createApp(App)
   .use(router)
